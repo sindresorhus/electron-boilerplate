8a268a99c4456945822ba715a4e1ce953549a4a4
Sindre Sorhus <sindresorhus@gmail.com>
2019-11-19 13:04:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const debug = require('electron-debug');
 const contextMenu = require('electron-context-menu');
 const config = require('./config');
 const menu = require('./menu');
-const {build} = require('./package.json');
+const packageJson = require('./package.json');
 
 unhandled();
 debug();
 contextMenu();
 
-app.setAppUserModelId(build.appId);
+app.setAppUserModelId(packageJson.build.appId);
 
 // Uncomment this before publishing your first version.
 // It's commented out as it throws an error if there are no published versions.
